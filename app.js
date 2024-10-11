69c4ddc033932f0a47e32a77669dcea8da54d80a
Afra Ibnat <107595433+afraibnat@users.noreply.github.com>
2024-10-11 17:25:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function changeColor() {
     element.classList.toggle("light-mode"); /* Toggle the "light-mode" class on the body element */
 
     const changeModeBtn = document.querySelector(".change-mode"); /* Select the button that changes the mode */
-    if (element.classList.contains("light-mode")) { /* Checks if the body currently has the "dark-mode" class */
+    if (element.classList.contains("light-mode")) { /* Checks if the body currently has the "light-mode" class */
         changeModeBtn.textContent = "Dark"; /* If clicked, change to "Dark" */
     } else {
         changeModeBtn.textContent = "Light"; /* If clicked, change to "Light" */
