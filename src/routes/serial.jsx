7f9ca15173f5e91aeba8895727a1d0dea001297f
nasun0 <aaron.chang384@gmail.com>
2025-10-30 14:06:15 -0500

    fixed import filename

--- src/routes/serial.jsx
+++ src/routes/serial.jsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Serial from "../pages/homepage/articles/serial/serialPage";
+import Serial from "../pages/homepage/articles/serial/SerialPage";
 
 export const Route = createFileRoute("/serial")({
   component: RouteComponent,
