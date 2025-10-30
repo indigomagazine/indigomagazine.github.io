909664e77c2628599885542491664dd11b724912
nasun0 <aaron.chang384@gmail.com>
2025-10-30 14:10:45 -0500

    changing import file names

--- src/routes/articles/serial/covet.jsx
+++ src/routes/articles/serial/covet.jsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import ArticlePage from "../../../pages/homepage/articles/serial/covet/covetPage";
+import ArticlePage from "../../../pages/homepage/articles/serial/covet/CovetPage";
 
 export const Route = createFileRoute("/articles/serial/covet")({
   component: RouteComponent,
